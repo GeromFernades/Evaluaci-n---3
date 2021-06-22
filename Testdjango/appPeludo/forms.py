33794847b51a5247934f3b46b1b2b552abbf66b7
GeromFernades <80848994+GeromFernades@users.noreply.github.com>
2021-06-22 18:42:29 -0400

    Add files via upload

---
+++ Testdjango/appPeludo/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.forms import ModelForm, fields
+from .models import Mascotas
+
+class MascotasForm(ModelForm):
+
+    class Meta:
+        model = Mascotas
+        fields=['codigo','nombre','especie','adoptado']
